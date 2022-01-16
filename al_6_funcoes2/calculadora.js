500c50625f8511f5c46102720859fb591bdfe203
Gustavo L Adas <gustavo_adas@hotmail.com>
2022-01-16 02:34:36 -0300

    confirmar em master

--- al_6_funcoes2/calculadora.js
+++ al_6_funcoes2/calculadora.js
@@ -9,31 +9,48 @@ let resultado;
 function soma() {
     resultado = n1 + n2;
     alert(`${n1} + ${n2} = ${resultado}`)
+    novaOperacao();
 }
 
 function subtracao() {
     resultado = n1 - n2;
     alert(`${n1} - ${n2} = ${resultado}`)
+    novaOperacao();
 }
 
 function multiplicacao() {
     resultado = n1 * n2;
     alert(`${n1} * ${n2} = ${resultado}`)
+    novaOperacao();
 }
 
 function divisaoReal() {
     resultado = n1 / n2;
     alert(`${n1} / ${n2} = ${resultado}`)
+    novaOperacao();
 }
-
 function divisaoInteira() {
     resultado = n1 % n2;
     alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`)
+    novaOperacao();
 }
 
 function potenciacao() {
     resultado = n1 ** n2;
     alert(`${n1} elevado a ${n2}ª é igual a ${resultado}`)
+    novaOperacao();
+}
+
+function novaOperacao() {
+    let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+    if (opcao == 1) {
+        calculadora();
+    } else id(opcao == 2) {
+        alert('Até mais!')
+    } else {
+        alert('Digite uma opção válida!')
+        novaOperacao();
+    }
 }
 
 if (operacao == 1) {
